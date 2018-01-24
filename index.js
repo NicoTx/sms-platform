8f9af850f6599132ec4ace03232f11e3214b3c77
nicolas <nicolas@nomail.com>
2018-01-24 12:53:01 +0700

    correction port

--- index.js
+++ index.js
@@ -13,12 +13,20 @@ async function startServer() {
     const app = new Koa()
     const router = new Router()
 
+    render(app, {
+        root: path.join(__dirname, 'views'),
+        layout: false,
+        viewExt: 'html',
+        cache: false,
+        debug: true
+      })
+      
     router.get('/', controller.start)
 
     app
       .use(router.routes())
       .use(router.allowedMethods())
-      .listen(SERVER_PORT)
+      .listen(process.env.SERVER_PORT)
 }
 
 startServer()
